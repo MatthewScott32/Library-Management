5040182eaeb8578ab69cf66b82fb1e714d61acc2
MatthewScott32 <matthewscottblagg@gmail.com>
2020-05-05 13:05:18 -0500

    book details

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import *
+from django.urls import path
 
 
 app_name = "libraryapp"
@@ -9,8 +10,9 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('', home, name='home'),
     path('books/', book_list, name='books'),
+    path('book/form', book_form, name='book_form'),
+    path('books/<int:book_id>/', book_details, name='book'),
     path('librarians/', librarians_list, name='librarians'),
     path('libraries/', library_list, name='libraries'),
-    path('book/form', book_form, name='book_form'),
     path('library/form', library_form, name='library_form'),
 ]
