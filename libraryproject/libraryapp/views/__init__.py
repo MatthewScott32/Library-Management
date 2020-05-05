2746e6c273414d42e813a0bea5192344de6b1e67
MatthewScott32 <matthewscottblagg@gmail.com>
2020-05-05 14:11:55 -0500

    edit books

--- libraryproject/libraryapp/views/__init__.py
+++ libraryproject/libraryapp/views/__init__.py
@@ -7,3 +7,4 @@ from .books.form import book_form
 from .libraries.form import library_form
 from .books.details import book_details
 from .libraries.details import library_details
+from .books.form import book_form, book_edit_form
